e4579584f739333c869df0facd11d67bd586f7fe
logesh chandran <logesh.c@krisopia.com>
2022-03-31 16:05:01 +0530

    Core project setup

---
+++ src/base.service.ts
@@ -0,0 +1,8 @@
+import { BehaviorSubject } from "rxjs";
+
+
+export abstract class BaseService {
+  abstract list$: BehaviorSubject<string[]>;
+  abstract addTodo(todo: string): void;
+  abstract removeTodo(index: number): void;
+}
